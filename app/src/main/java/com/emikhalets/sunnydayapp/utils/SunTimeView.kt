491739595c9988c6cb513dc6ece7150396cd1ae9
SuperCasual <evg.mikhalets@gmail.com>
2021-01-26 23:31:08 +0300

    drew drawable, placed text (added now time text), divided draw methods, added text size attr

--- app/src/main/java/com/emikhalets/sunnydayapp/utils/SunTimeView.kt
+++ app/src/main/java/com/emikhalets/sunnydayapp/utils/SunTimeView.kt
@@ -5,8 +5,13 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.RectF
+import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.content.withStyledAttributes
+import com.emikhalets.sunnydayapp.R
+
 
 class SunTimeView @JvmOverloads constructor(
     context: Context,
@@ -15,31 +20,34 @@ class SunTimeView @JvmOverloads constructor(
     defStyleRes: Int = 0
 ) : View(context, attrs, defStyleAttr, defStyleRes) {
 
+    private var image: Int = 0
+    private var textSize: Float = 0f
+
     private val linePaint = Paint(Paint.ANTI_ALIAS_FLAG)
-    private val sunPaint = Paint(Paint.ANTI_ALIAS_FLAG)
     private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-//    private var sunCanvas: Canvas? = null
+    private var sunDrawable: Drawable? = null
 
+    private var offset = 0f
     private var density = 0f
+    private var now: String = ""
     private var sunrise: String = ""
     private var sundown: String = ""
 
     init {
-        density = resources.displayMetrics.density
+        context.withStyledAttributes(attrs, R.styleable.SunTimeView) {
+            density = resources.displayMetrics.density
+            image = getResourceId(R.styleable.SunTimeView_android_src, 0)
+            textSize = getDimension(R.styleable.SunTimeView_android_textSize, 14 * density)
+        }
 
         linePaint.color = Color.BLACK
         linePaint.strokeWidth = 2 * density
         linePaint.style = Paint.Style.STROKE
 
-//        sunPaint.color = Color.BLACK
-
         textPaint.color = Color.BLACK
-        textPaint.textSize = 18 * density
+        textPaint.textSize = textSize
         textPaint.textAlign = Paint.Align.CENTER
-
-//        val sunBitmap = BitmapFactory.decodeResource(resources, R.drawable.ic_sun)
-//        sunCanvas = Canvas(sunBitmap.copy(Bitmap.Config.ARGB_8888, true))
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -51,33 +59,58 @@ class SunTimeView @JvmOverloads constructor(
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        drawView(canvas)
+        offset = 20 * density
+        drawLines(canvas)
+        drawText(canvas)
+        drawImage(canvas)
     }
 
-    private fun drawView(canvas: Canvas) {
-        val offset = 20 * density
-
+    private fun drawLines(canvas: Canvas) {
+        val lineHeight = height - offset
         val oval = RectF(
+            offset * 4,
             offset * 2,
-            offset,
-            width - offset * 2,
-            (height - offset) * 2
+            width - offset * 4,
+            (height - offset * 2) * 2
         )
+
         canvas.drawArc(oval, 180f, 180f, false, linePaint)
-
-        canvas.drawLine(
-            offset,
-            height - offset,
-            width - offset,
-            height - offset,
-            linePaint
-        )
-
-        canvas.drawText(sunrise, offset, height - offset, textPaint)
-        canvas.drawText(sundown, width - offset, height - offset, textPaint)
+        canvas.drawLine(offset, lineHeight, width - offset, lineHeight, linePaint)
     }
 
-    fun setTime(sunrise: String, sundown: String) {
+    private fun drawText(canvas: Canvas) {
+        if (now == "") now = "14:00"
+        if (sunrise == "") sunrise = "06:00"
+        if (sundown == "") sundown = "22:00"
+
+        val textOffset = 5 * density
+
+        canvas.drawText(now, (width / 2).toFloat(), offset, textPaint)
+        canvas.drawText(sunrise, offset * 2, height - offset - textOffset, textPaint)
+        canvas.drawText(sundown, width - offset * 2, height - offset - textOffset, textPaint)
+    }
+
+    private fun drawImage(canvas: Canvas) {
+        if (image != 0) {
+            val halfImage = (16 * density).toInt()
+            val ix = width / 2
+            val iy = (offset * 2).toInt()
+            sunDrawable = ResourcesCompat.getDrawable(resources, image, null)
+
+            sunDrawable?.setBounds(
+                ix - halfImage,
+                iy - halfImage,
+                ix + halfImage,
+                iy + halfImage
+            )
+            sunDrawable?.draw(canvas)
+        } else {
+            canvas.drawCircle((width / 2).toFloat(), offset * 2, 10 * density, linePaint)
+        }
+    }
+
+    fun setTime(now: String = "", sunrise: String = "", sundown: String = "") {
+        this.now = now
         this.sunrise = sunrise
         this.sundown = sundown
         invalidate()
